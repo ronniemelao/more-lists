0a7c895152a17e28da3048319e52fdf3250ced7e
ronniemelao <rmelao@ucvts.tec.nj.us>
2017-11-13 15:23:34 +0000

    list-practice.py

--- list-practice.py
+++ list-practice.py
@@ -5,6 +5,8 @@ If you're correct, you'll get a message in the terminal saying "Your string_list
 """
 
 # On the line below, create a list named string_list containing 7 strings
+
+#makes a list containing 7 strings
 string_list = ["Borneo", "The Australian Outback", "Africa", "Marquesas", "Thailand", "The Amazon", "Pearl Islands"]
 
 
